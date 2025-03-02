5565b013f713cc5c19432e9e91881d1b64eb05af
StivenGamba <jstivennicolas@gamil.com>
2025-03-02 00:43:57 -0500

    primer commit

---
+++ app/src/main/java/com/example/prueba_ext/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.prueba_ext
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/prueba_ext/splashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prueba_ext
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class splashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splahs) //Lo que se esta enviando para visualizar
+    }
+}
